279f931062978f26f1b8cbd56894625a057e506d
Benoit Hubert <benoithubert@gmail.com>
2019-09-17 11:17:02 +0200

    Build of initiation-javascript React app

---
+++ precache-manifest.cf579c8abf113b6d7ecde80136d1646e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e500767d00a9d91ee4beb1e2a2d1a408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fffeab54314f185dfb39",
+    "url": "/static/js/2.07340b49.chunk.js"
+  },
+  {
+    "revision": "ff58fbf4bde3a924615a",
+    "url": "/static/js/main.aa96c3e7.chunk.js"
+  },
+  {
+    "revision": "be5ddbfb8de796ec2724",
+    "url": "/static/js/runtime~main.707db749.js"
+  }
+]);
